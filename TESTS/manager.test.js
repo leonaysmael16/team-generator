a07718427d838aa330d7b71fb932880e2f4fcf63
leonaysmael16 <ysmealleona@gmail.com>
2021-10-26 23:21:23 -0700

    added and did testing

---
+++ TESTS/manager.test.js
@@ -0,0 +1,7 @@
+const { testing } = require('jest')
+const manager = require('./lin/manager.js')
+
+test('Testing for managers', ()=> {
+    const manager = new Manager('Matthew Bonner', '23','matt.bonner@company.com')
+    expect(mana)
+})
---
+++ index.js
@@ -0,0 +1,58 @@
+const employee = require('./lib/employee.js')
+const manager = require('./lib/manager.js')
+const engineer = require('./lib/engineer.js')
+const intern = require('./lib/intern.js')
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateHTML = require('./src/generateHTML')
+
+const teamArray =[ ]
+
+const generateManager = () => {
+    console.log('Please answer the questions to generate your team profile')
+    return inquirer.prompt([
+        {
+            name: 'name',
+            message: 'Who will be managing this team?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the manager name')
+                    return false
+                }
+            }
+        },
+        {
+           name: 'manager id',
+           message: 'What id number of the manager?',
+           validate: idInput => { 
+               if (idInput) {
+                   return true;
+               } else {
+                   console.log('Please enter the ID number of manager')
+                   return false
+               }
+           }
+        },
+        {
+            name: 'phone number',
+            message: 'What is the phone number of the manager?',
+            validate: numberInput => {
+                if (numberInput) {
+                    return true
+                } else {
+                    console.log('Please enter a phone number')
+                    return false
+                }
+            }
+        },
+        {
+            name: 'email' ,
+            message: ('What is the manager email?'),
+            validate: emailInput {
+                
+            }
+        }
+    ])
+}
