838b271265e7e8c07f2f6cd6acb94f0940327dd8
leonaysmael16 <ysmealleona@gmail.com>
2021-11-04 22:45:44 -0700

    added js and index.html

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const intern = require('./lib/intern.js')
 const inquirer = require('inquirer')
 const fs = require('fs')
 const generateHTML = require('./src/generateHTML')
+const Manager = require('./lib/manager.js')
+const { truncate } = require('fs/promises')
+const Engineer = require('./lib/engineer.js')
 
 const teamArray =[ ]
 
@@ -62,4 +65,75 @@ const generateManager = () => {
             }
         }
     ])
+    .then(managerInput => {
+        const {name, id, email, officeNumer} = managerInput
+        const manager = new Manager(name, id, email, officeNumer)
+        teamMembersArray.push(manager)
+        console.log(manager)
+        console.log('Successfully stored!')
+    })
+}
+
+const createEmployee = () => {
+    console.log('Please answer the following questions')
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What will be the employees role?',
+            choices: ['Intern', 'Engineer']
+        },
+        {
+            type:'name',
+            name: 'What is the name of the Employee?',
+            validate: nameInput => {
+                if(nameInput){
+                    return true
+                } else {
+                    console.log('Please enter name to continue')
+                    return false
+                }
+            }
+        },
+        {
+            name: 'school',
+            message: 'Where does the intern attend university?',
+            when: (input) => input.role == 'Intern',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true 
+                } else {
+                    console.log('Please enter school to enter')
+                }
+            }
+        },
+        {
+            name:'GitHub username',
+            message: "What is the GitHub username of Engineer",
+            when: (input) => input.role == 'Engineer',
+            validate: nameInput => {
+                if(nameInput){
+                    return true 
+                } else {
+                    console.log('Please enter username to continue')
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmCreateEmployee',
+            message: 'Would you like to add more to the team?',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        let {name, id, email, role, GitHub, school, confirmCreateEmployee} = employeeData
+        let employee
+        if(role === 'Engineer'){
+            employee = new Engineer(name, id, email, GitHub)
+            console.log(employee)
+        } else if(role === 'Intern'){
+            
+        }
+    })
 }
